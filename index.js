a69dfa0ef67f092c2838692d16fe2a4a2f4891d2
Tara Maddox <TMaddox85@gmail.com>
2022-12-12 10:51:43 -0800

    Update index.js
    I dont understand how to make this a function.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function newImage(greenCharacter){
-    let (greenCharacter) = document.createElement('img')
-    (greenCharacter).src = 'assets/green-character.gif'
-    (greenCharacter).style.position = 'fixed'
-    (greenCharacter).style.left = '100px'
-    (greenCharacter).style.bottom = '100px'
-    document.body.append(greenCharacter)
-    console.log( ${greenCharacter} )
+function newImage(url, left, bottom) {
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom +'px'
+    document.body.append(object)
+    console.log(object)
     
 }
-newImage(greenCharacter)
+newImage('assets/green-character.gif', 100, 100)
 /*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
