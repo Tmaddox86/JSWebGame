0878ed04892ff5b5af521565226b49dadbb845c1
Tara Maddox <TMaddox85@gmail.com>
2022-12-12 10:35:19 -0800

    tried to make functions

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-let greenCharacter = document.createElement('img')
+function newImage(greenCharacter){
+    let (greenCharacter) = document.createElement('img')
+    (greenCharacter).src = 'assets/green-character.gif'
+    (greenCharacter).style.position = 'fixed'
+    (greenCharacter).style.left = '100px'
+    (greenCharacter).style.bottom = '100px'
+    document.body.append(greenCharacter)
+    console.log( ${greenCharacter} )
+    
+}
+newImage(greenCharacter)
+/*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+
+let greenTree = document.createElement('img')
+greenTree.src = 'https://freesvg.org/img/Pine_Tree.png'
+greenTree.style.position = 'fixed'
+greenTree.style.left = '450px'
+greenTree.style.bottom = '200px'
+document.body.append(greenTree)
